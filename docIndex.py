e6def2344bd062cb6bfe75cb818e437d435907ba
rmace001 <rmace001@ucr.edu>
2019-02-03 03:43:27 -0800

    Create termIndex and docIndex

--- docIndex.py
+++ docIndex.py
@@ -35,14 +35,6 @@ for word in cleaned:
 end test"""
 
 
-#open stoplist.txt and store into a set
-stop_words_set = set()
-filepath = 'stoplist.txt'
-with open(filepath) as fp:
-    for line in fp:
-        for word in line.split():
-            stop_words_set.add(word)
-
 #unit test: print contents of files without printing stopwords
 """
 filepath = 'cs172data/*.txt'
@@ -57,6 +49,8 @@ for name in files:
                 if word not in stop_words_set:
                     print(word)
 """
+
+
 
 """
 #Create/print list of tuples
@@ -82,53 +76,6 @@ for item in termList:
 """
 
 
-# unit test: print each file to make sure there are no weird strings being encountred
-# unit test: use regex search to replace a '-' witha ' ' and split two-word terms
-# unit test: create a translator that converts punctuation to None or ""
-# function that filters uwantwed characters
-def filterSpecialChar(word):
-    sChars = [ '\x00' ,'\x00\x00' , '\x00\x00\x00' ]
-
-    if(word not in sChars):
-        return True
-    else:
-        return False
-
-dash = '-'
-http = 'http'
-translator = str.maketrans( '' , '' , string.punctuation)
-filepath = 'cs172data/file04.txt'
-files = glob.glob(filepath)
-fileID = 0
-termList = list()
-for name in files:
-    fileID += 1
-    with open(name) as fp:
-        for line in fp:
-            words = filter(filterSpecialChar , line.split())
-            for word in words:
-                if word not in stop_words_set:
-                    # check if '-' in word and 'http' not in word
-                    if re.search(dash, word) and not re.search(http, word):
-                        # replace dash with a space
-                        wordSet = word.replace( "-" , " " )
-                        # parse each word
-                        for term in wordSet.split():
-                            filtered_term = term.translate(translator)
-                            x = (filtered_term.lower(), fileID)
-                            termList.append(x)
-                    else:
-                        filtered_term = word.translate(translator)
-                        x = (filtered_term.lower(), fileID)
-                        termList.append(x)
-               # end if
-
-for item in termList:
-    #continue from here
-    print(item)
-
-
-
 """
 # unit test: use regex search to replace a '-' witha ' ' and split two-word terms
 testword = 'word-test'
@@ -143,7 +90,116 @@ if re.search(dash, testword):
     for term in wordSet.split():
         print(term)
 """
+# unit test: open stoplist.txt and store into a set
+stop_words_set = set()
+filepath = 'stoplist.txt'
+with open(filepath) as fp:
+    for line in fp:
+        for word in line.split():
+            stop_words_set.add(word)
 
 
+# unit test: print each file to make sure there are no weird strings being encountred
+# unit test: use regex search to replace a '-' witha ' ' and split two-word terms
+# unit test: create a translator that converts punctuation to None or ""
+# function that filters uwantwed characters
+def filterSpecialChar(word):
+    sChars = [ '\x00' ,'\x00\x00' , '\x00\x00\x00' ]
 
+    if(word not in sChars):
+        return True
+    else:
+        return False
+docIndex = dict()
+dash = '-'
+http = 'http'
+translator = str.maketrans( '' , '' , string.punctuation)
+filepath = 'cs172data/*.txt'
+files = glob.glob(filepath)
+fileID = 0
+termList = list()
+for name in files:
+    fileID += 1
+    with open(name) as fp:
+        count = 0
+        for line in fp:
+            words = filter(filterSpecialChar , line.split())
+            for word in words:
+                if word not in stop_words_set:
+                    # check if '-' in word and 'http' not in word
+                    if re.search(dash, word) and not re.search(http, word):
+                        # replace dash with a space
+                        wordSet = word.replace( "-" , " " )
+                        # parse each word
+                        for term in wordSet.split():
+                            count += 1
+                            filtered_term = term.translate(translator)
+                            x = (filtered_term.lower(), fileID)
+                            termList.append(x)
+                    else:
+                        count += 1
+                        filtered_term = word.translate(translator)
+                        x = (filtered_term.lower(), fileID)
+                        termList.append(x)
+        docIndex[fileID] = count
+
+def update_term(item, termIndex):
+    i = 0
+    for listitem in termIndex[item[0]]:
+        # if word_fileID == encountered_fileID
+        #for tempList in listitem:
+            if (item[1] == listitem[0]):
+                listitem[1] += 1
+                return
+            else:
+                i += 1
+    termIndex[item[0]].append( [item[1] , 1] )
+    return
+
+
+# item = (term, fileID)
+
+termIndex = dict()
+for item in termList:
+    if item[0] not in termIndex:
+        # add term
+        twoElemList = [item[1], 1]
+        newlist = [twoElemList]
+        termIndex[item[0]] = newlist
+    else:
+        # update term
+        
+        update_term(item, termIndex)
+        
+
+
+for key in termIndex.items():
+    print(key[0]+'->', end =" ")
+    for lists in key[1]:
+        print(lists)
+        # print( '[' + str(lists[0]) + ', ' + str(lists[1]) + ']'  )
+
+for key in docIndex.items():
+    print("Doc #" + str(key[0]) + "-> numTerms: " + str(key[1]))
+
+
+"""
+# unit test: check membership of dictionary key
+termIndex = dict()
+termIndex['yo'] = 23
+if 'yo' in termIndex:
+    print('true')
+if 'he' not in termIndex:
+    print('true')
+"""
+
+
+'''
+# unit test: append to listvalue of key-value pair 
+termIndex = dict()
+termIndex['ste'] = [ 1 , 1 ] 
+print(termIndex['ste'])
+termIndex['ste'].append(0)
+print(termIndex['ste'])
+'''
 
