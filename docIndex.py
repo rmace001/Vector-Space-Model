32e7502877428a0c45ed4baa0ed30c43cbe0def8
rmace001 <rmace001@ucr.edu>
2019-02-04 01:29:28 -0800

    Compute TF-IDF Weighting but Contains Stopwords Bug

--- docIndex.py
+++ docIndex.py
@@ -7,12 +7,10 @@ Created on Sun Jan 27 17:46:48 2019
 import glob
 import string
 import re
-
-
+import math
 """
-unit test: code that iterates through a directory of files file by file, line by line,
-word by word
-
+# unit test: code that iterates through a directory of files file by file, line by line,
+# word by word
 filepath = 'cs172testdata/*.txt'
 files = glob.glob(filepath)
 
@@ -28,15 +26,13 @@ for name in files:
 printable = set(string.printable)
 with open('cs172data/file20.txt', 'r') as content_file:
     content = content_file.read()
-    cleaned = filter(lambda x: x in printable, content)
-    
+    cleaned = filter(lambda x: x in printable, content)   
 for word in cleaned:
     print(word)
-end test"""
-
-
-#unit test: print contents of files without printing stopwords
 """
+
+"""
+#unit test: print contents of files without printing stopwords
 filepath = 'cs172data/*.txt'
 files = glob.glob(filepath)
 fileID = 0
@@ -50,11 +46,8 @@ for name in files:
                     print(word)
 """
 
-
-
 """
-#Create/print list of tuples
-
+#unit test: Create/print list of tuples
 filepath = 'cs172data/file06.txt'
 files = glob.glob(filepath)
 fileID = 0
@@ -75,21 +68,50 @@ for item in termList:
     print(item)
 """
 
-
 """
 # unit test: use regex search to replace a '-' witha ' ' and split two-word terms
 testword = 'word-test'
-
 if re.search(dash, testword):
     print("test")
     #replace dash with a space
-    wordSet = testword.replace( "-" , " " )
-    
+    wordSet = testword.replace( "-" , " " )    
     #parse each word and add to tmplist
     tmplist = list()
     for term in wordSet.split():
         print(term)
 """
+
+"""
+# unit test: check membership of dictionary key
+termIndex = dict()
+termIndex['yo'] = 23
+if 'yo' in termIndex:
+    print('true')
+if 'he' not in termIndex:
+    print('true')
+"""
+
+"""
+# unit test: append to listvalue of key-value pair 
+termIndex = dict()
+termIndex['ste'] = [ 1 , 1 ] 
+print(termIndex['ste'])
+termIndex['ste'].append(0)
+print(termIndex['ste'])
+"""
+
+"""
+# unit test: print each termInvertedIndex
+for key in termIndex.items():
+    print(key[0]+'->', end =" ")
+    for lists in key[1]:
+        print(lists)
+        # print( '[' + str(lists[0]) + ', ' + str(lists[1]) + ']'  )
+
+for key in docIndex.items():
+    print("Doc #" + str(key[0]) + "-> numTerms: " + str(key[1]))
+"""
+
 # unit test: open stoplist.txt and store into a set
 stop_words_set = set()
 filepath = 'stoplist.txt'
@@ -99,9 +121,7 @@ with open(filepath) as fp:
             stop_words_set.add(word)
 
 
-# unit test: print each file to make sure there are no weird strings being encountred
-# unit test: use regex search to replace a '-' witha ' ' and split two-word terms
-# unit test: create a translator that converts punctuation to None or ""
+
 # function that filters uwantwed characters
 def filterSpecialChar(word):
     sChars = [ '\x00' ,'\x00\x00' , '\x00\x00\x00' ]
@@ -110,6 +130,11 @@ def filterSpecialChar(word):
         return True
     else:
         return False
+
+
+# unit test: print each file to make sure there are no weird strings being encountred
+# unit test: use regex search to replace a '-' witha ' ' and split two-word terms
+# unit test: create a translator that converts punctuation to None or ""
 docIndex = dict()
 dash = '-'
 http = 'http'
@@ -147,18 +172,15 @@ def update_term(item, termIndex):
     i = 0
     for listitem in termIndex[item[0]]:
         # if word_fileID == encountered_fileID
-        #for tempList in listitem:
-            if (item[1] == listitem[0]):
-                listitem[1] += 1
-                return
-            else:
-                i += 1
+        if (item[1] == listitem[0]):
+            listitem[1] += 1
+            return
+        else:
+            i += 1
     termIndex[item[0]].append( [item[1] , 1] )
     return
 
-
 # item = (term, fileID)
-
 termIndex = dict()
 for item in termList:
     if item[0] not in termIndex:
@@ -168,38 +190,34 @@ for item in termList:
         termIndex[item[0]] = newlist
     else:
         # update term
-        
         update_term(item, termIndex)
-        
 
 
-for key in termIndex.items():
-    print(key[0]+'->', end =" ")
-    for lists in key[1]:
-        print(lists)
-        # print( '[' + str(lists[0]) + ', ' + str(lists[1]) + ']'  )
 
-for key in docIndex.items():
-    print("Doc #" + str(key[0]) + "-> numTerms: " + str(key[1]))
-
-
-"""
-# unit test: check membership of dictionary key
-termIndex = dict()
-termIndex['yo'] = 23
-if 'yo' in termIndex:
-    print('true')
-if 'he' not in termIndex:
-    print('true')
-"""
+def display_term_data(termIndex, docIndex, term):
+    print(term + '-> ')
+    if term in termIndex:
+        for postings in termIndex[term]:
+            #what items to i need to compute tf, idf, & tf-idf?
+            tf = postings[1]/docIndex[postings[0]] #num_occur/doc_size
+            idf = math.log(20/len(termIndex[term]), 2)
+            tf_idf = tf*idf
+            print('for this posting, tf: ' + str(tf) + ', idf: ' + str(idf) + ', tf-idf: ' + str(tf_idf))
+    else:
+        print('Term not found')
 
 
-'''
-# unit test: append to listvalue of key-value pair 
-termIndex = dict()
-termIndex['ste'] = [ 1 , 1 ] 
-print(termIndex['ste'])
-termIndex['ste'].append(0)
-print(termIndex['ste'])
-'''
+            
+
+term = 'phone'
+
+
+s = input()
+while s != 'sudoquit':
+    print(s)
+    display_term_data(termIndex, docIndex, s)
+    s = input()
+
+
+
 
